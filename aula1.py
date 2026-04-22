91bcadefb42ed839173c3dfd9ee280f30b8d7cf0
Vitorio <vitorio-augusto@hotmail.com>
2026-04-22 10:26:19 -0300

    inicio estudos adiconando as aulas

---
+++ aula1.py
@@ -0,0 +1,5 @@
+age = 25 
+
+print(f"idade =",age)
+
+print('Hello World!!')
---
+++ aula2.py
@@ -0,0 +1,8 @@
+nome = 'vitorio'
+
+n1 = 10 
+n2 = 10 
+
+resultado = n1 + n2
+
+print(f"nome:" ,nome, "resultado da soma é igual:" ,resultado)
