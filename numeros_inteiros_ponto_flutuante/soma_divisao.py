4afb1e3a0d5c6f7c9c107a739d5402d64fff4d4c
Vitorio <vitorio-augusto@hotmail.com>
2026-04-23 19:48:34 -0300

    modulo de adição e subtração / divisão

---
+++ numeros_inteiros_ponto_flutuante/soma_divisao.py
@@ -0,0 +1,20 @@
+numero1 = 40
+numero2 = 50
+
+soma = numero1 + numero2
+print(soma)
+
+divisao = numero1 / numero2
+print(divisao)
+
+numero_float1 = 4.5
+numero_float2 = 76.2
+
+soma_float = numero_float1 + numero_float2
+divisao_float = numero_float1 / numero_float2
+print(soma_float)
+print(divisao_float)
+
+print(type(soma))
+print(type(divisao_float))
+
