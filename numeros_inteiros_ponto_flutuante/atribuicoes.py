8424a1bb09b4d20c281c32a8ec4b5e938b3ac3ec
Vitorio <vitorio-augusto@hotmail.com>
2026-04-24 14:39:48 -0300

    operações

---
+++ numeros_inteiros_ponto_flutuante/atribuicoes.py
@@ -0,0 +1,49 @@
+numero1 = 10 
+numero1 += 5 
+
+print(numero1)
+
+
+count = 14
+count -= 3
+
+print(count) # 11
+
+product = 65
+product *= 7
+
+print(product) # 455
+
+price = 100
+price /= 4
+
+print(price) # 25.0
+
+
+total_pages = 23
+total_pages //= 5
+
+print(total_pages) # 4
+
+
+bits = 35
+bits %= 2
+
+print(bits) # 1
+
+
+power = 2
+power **= 3
+
+print(power) # 8
+
+greet = 'Hello'
+greet += ' World'
+
+print(greet) # Hello World
+
+
+greet = 'Hello'
+greet *= 3
+
+print(greet) # HelloHelloHello
