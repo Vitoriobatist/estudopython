73498bd03418bf609eae5c1e2d44c33c7b946a31
Vitorio <vitorio-augusto@hotmail.com>
2026-04-26 01:11:23 -0300

    adicionando mais um workshop

---
+++ booleanos_condicionais/true_false.py
@@ -0,0 +1,17 @@
+#print(bool(False)) # False
+#print(bool(1))  # False
+#print(bool('')) # False
+
+#print(bool(True)) # True
+#print(bool(1)) # True
+#print(bool('Hello')) # True
+
+
+voto = False
+idade = 19
+
+
+if voto or idade >= 18: # as duas variaves precisam ser true para cair na condição verdadeira. 
+    print("voce pode votar")
+else:
+    print("voce não pode votar")
